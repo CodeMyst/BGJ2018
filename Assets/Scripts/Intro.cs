6a239b1cb512efe89ea3d4e310696f3ae7f64d1e
CodeMyst <codemyst@outlook.com>
2018-08-31 17:18:00 +0200

    Added intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace BGJ2018
+{
+    public class Intro : MonoBehaviour
+    {
+        [SerializeField]
+        [Multiline]
+        private string [] intro;
+
+        private float characterTime = 0.025f;
+        private float paragraphTime = 3f;
+        [SerializeField]
+        private TextMeshProUGUI textUI;
+
+        private void Start ()
+        {
+            StartCoroutine (DisplayIntro ());
+        }
+
+        private IEnumerator DisplayIntro ()
+        {
+            foreach (string p in intro)
+            {
+                textUI.SetText ("");
+
+                int index = 0;
+
+                do
+                {
+                    textUI.SetText (textUI.text + p [index]);
+                    index++;
+                    yield return new WaitForSeconds (characterTime);
+                } while (index + 1 <= p.Length);
+
+                yield return new WaitForSeconds (paragraphTime);
+            }
+
+            FindObjectOfType<SceneLoader> ().LoadNextScene ();
+        }
+    }
+}
